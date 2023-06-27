b377fade8f0cb476d9f95cf2d0a360888248a7d5
zhaowei <zw204801@163.com>
2023-06-27 16:45:05 +0800

    feat: update web3mq-react dark mode

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,6 +1,6 @@
 export default [
-  require('/Users/coder/WorkSpace/Documentation/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/coder/WorkSpace/Documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/coder/WorkSpace/Documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
-  require('/Users/coder/WorkSpace/Documentation/src/css/custom.css'),
+  require('/Users/zhaowei/Desktop/trunk/ty/Documentation/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/zhaowei/Desktop/trunk/ty/Documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/zhaowei/Desktop/trunk/ty/Documentation/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/zhaowei/Desktop/trunk/ty/Documentation/src/css/custom.css'),
 ];
